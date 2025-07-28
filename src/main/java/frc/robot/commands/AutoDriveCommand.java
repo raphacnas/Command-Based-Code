c38620b9e57f3707b4495c8c98ef0cd9c240824d
raphacnas <raphacnas@gmail.com>
2025-07-28 14:30:57 -0300

    Correção de lógica do autônomo

--- src/main/java/frc/robot/commands/AutoDriveCommand.java
+++ src/main/java/frc/robot/commands/AutoDriveCommand.java
@@ -5,16 +5,22 @@ import java.util.TooManyListenersException;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class AutoDriveCommand extends Command {
 
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
   private DriveSubsystem SubSys;
   private final double distance;
   Timer timer = new Timer();
+
+  double tx, ty, ta;
 
   public AutoDriveCommand(DriveSubsystem SubSys, double distance) {
 
@@ -30,12 +36,23 @@ public class AutoDriveCommand extends Command {
   @Override
   public void execute() {
     timer.start();
-
+   
     if (timer.get() < 3){
       SubSys.setMotorSpeeds(distance, distance);
     } else {
       SubSys.setMotorSpeeds(0, 0);
+      limelight();
     }
+  }
+
+  public void limelight() {
+    tx = table.getEntry("tx").getDouble(0.0);
+    ty = table.getEntry("ty").getDouble(0.0);
+    ta = table.getEntry("ta").getDouble(0.0);
+
+    SmartDashboard.putNumber("*** - Tx", tx);
+    SmartDashboard.putNumber("*** - Ty", ty);
+    SmartDashboard.putNumber("*** - Ta", ta);
   }
 
   @Override
